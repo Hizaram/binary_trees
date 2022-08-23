1a0258b4f257f046ed5a96bd9f5409a4ac5f4822
Hizaram <promisevector@gmail.com>
2022-08-23 20:07:53 +0100

    Added the function that computes and returns the number of nodes with at least one child in a binary tree

---
+++ 13-binary_tree_nodes.c
@@ -0,0 +1,39 @@
+#include "binary_trees.h"
+
+/**
+ * node_count - counts the number of nodes with at least one child
+ * @tree: the binary tree in question
+ * @n: a pointer to the tree's non-leaf count
+ */
+
+void node_count(const binary_tree_t *tree, size_t *n)
+{
+	if (tree != NULL)
+	{
+		if ((tree->left == NULL) || (tree->right == NULL))
+		{
+			if (n != NULL)
+			{
+				(*n)++;
+				node_count(tree->left, n);
+				node_count(tree->right, n);
+			}
+		}
+	}
+}
+
+/**
+ * binary_tree_nodes - Returns the number of nodes with at least one \
+ * child in a binary tree
+ * @tree: Binary tree in question
+ *
+ * Return: number of nodes with at least one child in a given binary tree
+ */
+
+size_t binary_tree_nodes(const binary_tree_t *tree)
+{
+	size_t nodes = 0;
+
+	node_count(tree, &nodes);
+	return (nodes);
+}
