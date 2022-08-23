992954449b9364b3999d9356a9b8e86d1992ff5c
Hizaram <promisevector@gmail.com>
2022-08-23 11:07:33 +0100

    Fixed the recursive function

--- 8-binary_tree_postorder.c
+++ 8-binary_tree_postorder.c
@@ -3,7 +3,7 @@
 /**
  * binary_tree_postorder - traverses a binary tree with post-order funct
  * @tree: tree to be traversed
- * @func: the preoder function
+ * @func: the postorder function
  */
 
 void binary_tree_postorder(const binary_tree_t *tree, void (*func)(int))
@@ -12,8 +12,8 @@ void binary_tree_postorder(const binary_tree_t *tree, void (*func)(int))
 	{
 		if (func != NULL)
 		{
-			binary_tree_postorder(tree->left);
-			binary_tree_postorder(tree->right);
+			binary_tree_postorder(tree->left, func);
+			binary_tree_postorder(tree->right, func);
 			func(tree->n);
 		}
 	}
